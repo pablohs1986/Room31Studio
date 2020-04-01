fa08903e2796dbc82d01922787dae0a05b47d230
Pablo Herrero <pabloherrero1986@gmail.com>
2020-04-01 11:53:58 +0200

    Changing standar container width to 1190px

--- js/room.js
+++ js/room.js
@@ -28,7 +28,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 730) {
+    if ($("#mainNav").offset().top > 723) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
